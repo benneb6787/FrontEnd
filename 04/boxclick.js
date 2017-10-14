dc000cffb616779269eeb753171eb4d46a16aad2
benneb6786 <benneb6786@gmail.com>
2017-10-14 20:35:35 +0900

    05

--- 04/boxclick.js
+++ 04/boxclick.js
@@ -3,27 +3,58 @@ var b = document.getElementById('level');
 var c = document.getElementById('miss');
 var d = document.getElementById('misstotal');
 var box = document.getElementById('wrap3');
-a.innerHTML=1;
-b.innerHTML=2;
-c.innerHTML=3;
-d.innerHTML=4;
-
 var lco, rco;
-var str ='';
+var score = 0, level = 1, miss = 0, misstotal = 10, sec = 1500, change = 0, turn = 0, levelup = 5;
+
+a.innerHTML=score;
+b.innerHTML=level;
+c.innerHTML=miss;
+d.innerHTML=misstotal;
+
 setInterval(function(){
-  lco = Math.floor(Math.random()*700) + 150;
-  rco = Math.floor(Math.random()*700) + 150;
-
-  box.innerHTML += '<div id="wrap3" style="margin-left: "'+lco+'"px; margin-top: "'+rco+'"px"></div>';
-
-  console.log(lco,rco);
-}, 1000);
-
-
-var str='';
-document.addEventListener('click', function(event) {
-  if(event.target.id == 'box'){
-    console.log('ss');
-    box.remove();
+  turn++;
+  if(change == 0){        //아무것도 눌리지 않았을 경우 change = 0이 되서 miss를 증가시켜 줌
+      miss++;
+      c.innerHTML=miss;
   }
+
+  if(score == levelup){   //같은 level에서 miss될 경우 level이 오르지 않게 하기 위해 levelup을 만듦
+    level++; misstotal = misstotal - 2; sec = sec - 130; miss = 0; levelup += 5;
+    b.innerHTML=level;
+    c.innerHTML=miss;
+    d.innerHTML=misstotal;
+  }
+
+  if(miss == misstotal + 1) {
+    alert("GAME OVER");
+    reset();
+  }
+
+  if(level == 6){         //최대 level은 5까지!
+    alert("Congratulation");
+    reset();
+  }
+
+  lco = Math.floor(Math.random()*200) + 150;
+  rco = Math.floor(Math.random()*1500) + 150;
+  box.style.backgroundColor = 'red';
+  box.style.margin = lco+' '+rco;
+  change = 0;
+  console.log(turn, miss, score);
+}, sec);                  //level이 올라갈 때마다 시간이 줄어들게 하기위해 sec을 만듦
+
+function reset(){         //승리하거나 실패 시 처음부터 다시 하도록 설정
+  count = 0; score = 0; level = 1; miss = 0; misstotal = 10; sec = 1500;
+}
+
+document.addEventListener('mousedown', function(event) {
+  if(event.target.id == "wrap3" && score < (turn - miss + 1)){      //연속적인 클릭을 방지하기 위해 turn을 사용
+    box.style.backgroundColor = 'blue';
+    score++;
+    change = 1;
+    a.innerHTML=score;
+  }
+});
+document.addEventListener('mouseup', function(event) {            //마우스를 누르고 땠을 경우 다시 빨간색으로 돌아옴으로 이벤트를 발생시킴
+  if(box.style.backgroundColor = 'blue') {box.style.backgroundColor = 'red';}
 });
